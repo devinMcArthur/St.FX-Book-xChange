d65bc78640b48c4c998dc0477ce7aa57d49af2af
devinMcArthur <assortedgerm@gmail.com>
2016-11-08 14:03:25 -0400

    Git init on reset computer

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "St.FX Book xChange"
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
+  end
+
+end
