60d662418623d39f62807719d575d9bff5b4b835
devinMcArthur <assortedgerm@gmail.com>
2016-11-14 19:14:50 -0400

    Implemenet remember me functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130314184954) do
+ActiveRecord::Schema.define(version: 20161114221026) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20130314184954) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "remember_token"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["remember_token"], name: "index_users_on_remember_token"
+    t.index ["remember_digest"], name: "index_users_on_remember_digest"
   end
 
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -24,7 +24,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", login_path,  count: 0
+    assert_select "a[href=?]", signup_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
@@ -36,5 +37,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_not cookies['remember_token']
   end
 end
