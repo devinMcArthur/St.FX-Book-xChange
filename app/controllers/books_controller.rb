8697fa9096231657f51a9fbfc73b017a9fec4363
devinMcArthur <assortedgerm@gmail.com>
2016-11-29 17:25:28 -0400

    Initialize book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      flash[:success] = "Your book has been added!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+  def show
+    @books = current_user.books.build if logged_in?
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title)
+    end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @book = books(:matrix_algebra)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Book.count' do
+      post books_path, params: { book: { title: "Elementary Linear Algebra" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Book.count' do
+      delete book_path(@book)
+    end
+    assert_redirected_to login_url
+  end
 end
