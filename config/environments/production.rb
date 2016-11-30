8319498dd4660eb844820528d6326e74dea34c1b
devinMcArthur <assortedgerm@gmail.com>
2016-11-29 23:35:11 -0400

    Another attempt to fix production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app54894855@heroku.com'],
-    :password       => ENV['9assr8iv4943'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
