270e0eced9ec41efc722214b07d7c4fd5e8519af
Devin McArthur <assortedgerm@gmail.com>
2016-12-16 20:39:02 -0400

    Fix book title problem

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -17,7 +17,7 @@ class ConversationsController < ApplicationController
 
   private
     def conversation_params
-      params.permit(:sender_id, :recipient_id)
+      params.permit(:sender_id, :recipient_id, :book_id)
     end
 
     def correct_user
