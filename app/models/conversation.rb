9cfbeeb9172e121171c9893c81214ee3310ace2a
Devin McArthur <assortedgerm@gmail.com>
2016-12-16 21:49:57 -0400

    Sender can now start multiple conversations with the same user

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,11 +5,11 @@ class Conversation < ActiveRecord::Base
   belongs_to :recipient, :foreign_key => :recipient_id, class_name: 'User'
   belongs_to :book, :foreign_key => :book_id, class_name: 'Book'
   has_many :messages, dependent: :destroy
-  validates_uniqueness_of :sender_id, :scope => :recipient_id
+  #validates_uniqueness_of :sender_id, :scope => :recipient_id
 
   scope :between, -> (sender_id, recipient_id, book_id) do
     where("(conversations.sender_id = ? AND conversations.recipient_id = ? AND conversations.book_id = ?) OR
-           (conversations.recipient_id = ? AND conversations.sender_id = ? AND conversations.book_id = ?)",
+           (conversations.sender_id = ? AND conversations.recipient_id = ? AND conversations.book_id = ?)",
            sender_id, recipient_id, book_id, recipient_id, sender_id, book_id)
   end
 end
