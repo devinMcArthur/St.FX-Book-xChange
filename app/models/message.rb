ee989ddff251533731fafb94d91f26c9e7e802bd
devinMcArthur <assortedgerm@gmail.com>
2017-01-23 17:25:49 -0400

    Remove notification changes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
-  has_many :notifications, as: :event
 
   #after_create :send_notification
   validates_presence_of :body, :conversation_id, :user_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :conversations, dependent: :destroy
   has_many :messages
   has_many :messages_received, class_name: 'Message', foreign_key: :to_id
-  has_many :notifications
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
