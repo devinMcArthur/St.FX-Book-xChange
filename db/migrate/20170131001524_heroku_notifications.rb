14b9e81398f6878ce49e16543fed47c14e81783c
devinMcArthur <assortedgerm@gmail.com>
2017-01-30 20:45:48 -0400

    Heroku notifications

--- db/migrate/20170131001524_heroku_notifications.rb
+++ db/migrate/20170131001524_heroku_notifications.rb
@@ -1,13 +1,16 @@
 class HerokuNotifications < ActiveRecord::Migration[5.0]
   def change
     create_table :notifications do |t|
-      t.integer :sender_id, index: true
-      t.integer :user_id, index: true
-      t.integer :conversation_id, index: true
+      t.references :sender_id, index: true
+      t.references :user_id, index: true
+      t.references :conversation_id, index: true
       t.integer :message_id
       t.boolean :read, default: false
 
       t.timestamps null: false
     end
+    add_foreign_key :notifications, :users
+    add_foreign_key :notifications, :users, column: :user_id
+    add_foreign_key :notifications, :conversations, column: :conversation_id
   end
 end
