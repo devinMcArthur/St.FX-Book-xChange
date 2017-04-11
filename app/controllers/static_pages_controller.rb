366f9a0603ff49f4c233b88ae41b43c0ae97fcdd
devinMcArthur <assortedgerm@gmail.com>
2017-04-11 17:24:20 -0300

    Change Home page flash

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    flash.now[:info] = "Please keep in mind that this site is still in Beta"
+    flash.now[:info] = "Please keep in mind that this site is still in Beta, check the Contact page to report any issues!"
   end
 
   def help
