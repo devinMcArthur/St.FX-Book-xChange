e3298593da976b9e1219d0d94d71f012b6363182
Devin McArthur <assortedgerm@gmail.com>
2017-07-23 18:49:17 -0600

    Make search case insensitive

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   has_many :conversations, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   # For searching
-  scope :title, -> (title) { where "title LIKE ?","%#{title}%"}
+  scope :title, -> (title) { where "title LIKE ?","%#{title.downcase}%"}
   validates :user_id, presence: true
   validates :title,   presence: true, length: { maximum: 100 }
 
