aa9d5958b63d8130c6e50dac03e484380135ce95
devinMcArthur <assortedgerm@gmail.com>
2017-10-23 19:56:01 -0300

    Robot.txt and fix push issues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,13 +3,9 @@ class StaticPagesController < ApplicationController
   after_action  :temporary_delete, only: [:home], unless: -> { !@tempDemands.nil? }
 
   def home
-<<<<<<< HEAD
     @demands = Demand.where(temporary: true).last
     if @demand = Demand.new
     end
-=======
-    flash.now[:info] = "Please keep in mind that this site is still in Beta, so there will be plently of upcoming features and fixes!"
->>>>>>> 9a47677b0a178dcf7d9dd7ac0f48ee5202eaf8b6
   end
 
   def help
